44240a5dcdddfe6abb4c3988c1a2d8825ad9532a
Mironau <mironiu93@gmail.com>
2022-06-21 09:38:56 +0300

    filling list with 2 ** n

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Python")
+# word = input('Введите слово: ')
+# word = word.lower()
+# word1 = word[:]
+# word2 = word[: : -1]
+# result = word1 == word2
+# print(result)
+
+words = input('Введите несколько слов: ')
+words = words.lower()
+words = words.split()
+print(words)
+words_new =
