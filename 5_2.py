d235dbd39da2dd6452e2152202b9df2980a0d6c1
Mironau <mironiu93@gmail.com>
2022-06-24 15:17:01 +0300

    5_2_calculator_update. added sqrt

--- 5_2.py
+++ 5_2.py
@@ -1,26 +1,32 @@
+math_operation = input(
+    'Какое действие произвести с числом (1 : сложить, 2 : отнять, 3 : умножить, 4 :разделить, 5: возвести в степень, 6: извлечь квадратный корень числа)? Введите число от 1 до 6:  ')
+while (int(math_operation) < 1 or int(math_operation) > 6) or not math_operation.isdigit():
+    math_operation = input(
+        'Какое действие произвести с числом (1 : сложить, 2 : отнять, 3 : умножить, 4 :разделить, 5: возвести в степень, 6: извлечь квадратный корень числа)? Введите число от 1 до 6:  ')
 first_digit = input('Введите число 1 (число должно быть целым): ')
 while not first_digit.isdigit():
     first_digit = input('Введите число 1 (число должно быть целым): ')
-second_digit = input('Введите число 2 (число должно быть целым): ')
-while not second_digit.isdigit():
+if int(math_operation) == 6:
+    print(int(first_digit) ** 0.5)
+else:
     second_digit = input('Введите число 2 (число должно быть целым): ')
-math_operation = input('Какое действие произвести с числом (1 : сложить, 2 : отнять, 3 : умножить, 4 :разделить, 5: возвести в степень)? Введите число от 1 до 5:  ')
-while (int(math_operation) < 1 or int(math_operation) > 5) or not math_operation.isdigit():
-    math_operation = input(
-        'Какое действие произвести с числом (1 : сложить, 2 : отнять, 3 : умножить, 4 :разделить, 5: возвести в степень)? Введите число от 1 до 5:  ')
-if int(math_operation) == 1:
-    print(int(first_digit) + int(second_digit))
-elif int(math_operation) == 2:
-    print(int(first_digit) - int(second_digit))
-elif int(math_operation) == 3:
-    print(int(first_digit) * int(second_digit))
-elif int(math_operation) == 4:
-    while int(second_digit) == 0:
-        second_digit = input('Введите число 2 (число должно быть целым и отличным от нуля): ')
     while not second_digit.isdigit():
-        second_digit = input('Введите число 2 (число должно быть целым и отличным от нуля): ')
-    print(int(int(first_digit) / int(second_digit)))
-elif int(math_operation) == 5:
-    print(int(first_digit) ** int(second_digit))
+        second_digit = input('Введите число 2 (число должно быть целым): ')
+    if int(math_operation) == 1:
+        print(int(first_digit) + int(second_digit))
+    elif int(math_operation) == 2:
+        print(int(first_digit) - int(second_digit))
+    elif int(math_operation) == 3:
+        print(int(first_digit) * int(second_digit))
+    elif int(math_operation) == 4:
+        while int(second_digit) == 0:
+            second_digit = input('Введите число 2 (число должно быть целым и отличным от нуля): ')
+        while not second_digit.isdigit():
+            second_digit = input('Введите число 2 (число должно быть целым и отличным от нуля): ')
+        print(int(int(first_digit) / int(second_digit)))
+    elif int(math_operation) == 5:
+        print(int(first_digit) ** int(second_digit))
+    elif int(math_operation) == 6:
+        print(int(first_digit) ** 0.5)
 
 
