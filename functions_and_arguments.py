d30fe19472d2ad1a49c5ca35a5d9ba96c7e1dbde
Mironau <mironiu93@gmail.com>
2022-06-30 09:02:56 +0300

    6_1

--- functions_and_arguments.py
+++ functions_and_arguments.py
@@ -17,11 +17,13 @@ def foo(a: int, b: list = []) -> list:
 print(foo(5))
 print(foo(4))
 
-numbers = [1, 2, 3, 4, [5, 6, [7,8,9,[3,2,3,]]]]
+numbers = [1, 2, 3, 4, [5, 6, [7, 8, 9, [3, 2, 3]]]]
+
 
 def multipy(numbers: list) -> int:
     counter = 1
     for number in numbers:
+
         if isinstance(number, int):
             counter += number
         else:
