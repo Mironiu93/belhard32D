f3415a70ef0d4d90e58b34d86103463a0bb560df
Mironau <mironiu93@gmail.com>
2022-07-06 15:00:47 +0300

    8_1_with_input

--- 8_1.py
+++ 8_1.py
@@ -1,8 +1,10 @@
+depth = int(input("Введите желаемую глубину треугольника Паскаля: "))
+
 def PascalTriangle(raws):
     raw = [1]
     for i in range(raws):
         print(raw)
         raw = [sum(x) for x in zip([0] + raw, raw + [0])]
+    return raw
 
-
-PascalTriangle(20)
+PascalTriangle(depth)
